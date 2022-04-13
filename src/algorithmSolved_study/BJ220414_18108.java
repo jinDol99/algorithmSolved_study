8c90dc302a6f722c917529a192488fc5d4f82686
WDJJH <jeonjonghun0011@gmail.com>
2022-04-14 01:02:41 +0900

    18108번

---
+++ src/algorithmSolved_study/BJ220414_18108.java
@@ -0,0 +1,11 @@
+package algorithmSolved_study;
+import java.util.Scanner;
+public class BJ220414_18108 {
+	public class Main {
+	    public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        int y = s.nextInt();
+	        System.out.println(y-543);
+	    }
+	}
+}
