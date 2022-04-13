2096f9ef738cb24fc262016c5f228f5ad68509ed
parkm <parkm@169.254.140.98>
2022-04-14 00:47:25 +0900

    22년 4월 14일 박진석 15727번 문제 학습완료

---
+++ src/algorithmSolved_study/BJ220414_15727.java
@@ -0,0 +1,15 @@
+package algorithmSolved_study;
+import java.util.Scanner;
+
+public class BJ220414_15727 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("");
+		int L = sc.nextInt();
+		
+		if(L % 5 == 0)
+			System.out.println(L / 5);
+		else 
+			System.out.println((L / 5) + 1);
+	}
+}
