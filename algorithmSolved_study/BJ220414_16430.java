1c49d32a4e11ccc988cd495ed837e85d16aad207
WDJJH <jeonjonghun0011@gmail.com>
2022-04-15 00:41:07 +0900

    10172번인데 여기 올리기

--- algorithmSolved_study/BJ220414_16430.java
+++ algorithmSolved_study/BJ220414_16430.java
@@ -1,11 +1,11 @@
 package algorithmSolved_study;
 
 public class BJ220414_16430 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("¾ËÄÝ ¸¶·Æ´Ù.");
-
-	}
-
+    public static void main(String[] args) {
+        System.out.print("|\\_/|\n");
+        System.out.print("|q p|   /}\n");
+        System.out.print("( 0 )\"\"\"\\\n");
+        System.out.print("|\"^\"`    |\n");
+        System.out.println("||_/=\\\\__|");
+    }
 }
