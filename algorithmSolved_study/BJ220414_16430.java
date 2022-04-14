fef6e1c7c8a973732850a378a0919f06786fccb7
김건휘 <@DESKTOP-S73ASEP>
2022-04-15 00:46:00 +0900

    끝

--- algorithmSolved_study/BJ220414_16430.java
+++ algorithmSolved_study/BJ220414_16430.java
@@ -1,11 +1,12 @@
 package algorithmSolved_study;
 
+import java.util.Scanner;
+
 public class BJ220414_16430 {
     public static void main(String[] args) {
-        System.out.print("|\\_/|\n");
-        System.out.print("|q p|   /}\n");
-        System.out.print("( 0 )\"\"\"\\\n");
-        System.out.print("|\"^\"`    |\n");
-        System.out.println("||_/=\\\\__|");
+    	Scanner sc = new Scanner(System.in);
+	    int a = sc.nextInt();
+	    int b = sc.nextInt();
+	    System.out.println(b-a + " " +b);
     }
 }
