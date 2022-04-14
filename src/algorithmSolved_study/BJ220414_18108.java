f67d05067db793cb598a1de806ec1b52abcce4d0
WDJJH <jeonjonghun0011@gmail.com>
2022-04-14 23:46:18 +0900

    18108

---
+++ src/algorithmSolved_study/BJ220414_18108.java
@@ -0,0 +1,9 @@
+package algorithmSolved_study;
+import java.util.Scanner;
+public class BJ220414_18108 {
+	    public static void main(String[] args) {
+	        Scanner s = new Scanner(System.in);
+	        int y = s.nextInt();
+	        System.out.println(y-543);
+	   }
+}
