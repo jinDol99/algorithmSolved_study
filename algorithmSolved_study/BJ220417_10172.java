61aba575a1e88ee1a380171e0747005e00a2a039
WDJJH <jeonjonghun0011@gmail.com>
2022-04-17 03:44:27 +0900

    10172

---
+++ algorithmSolved_study/BJ220417_10172.java
@@ -0,0 +1,10 @@
+package algorithmSolved_study;
+public class BJ220417_10172 {
+    public static void main(String[] args) {
+        System.out.print("|\\_/|\n");
+        System.out.print("|q p|   /}\n");
+        System.out.print("( 0 )\"\"\"\\\n");
+        System.out.print("|\"^\"`    |\n");
+        System.out.println("||_/=\\\\__|");
+    }
+}
