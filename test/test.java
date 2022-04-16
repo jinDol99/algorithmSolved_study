2ad25eadb92a816142bf781a9f9cd793781b2dec
USER <lee99714@naver.com>
2022-04-17 02:21:33 +0900

    asdf

--- test/test.java
+++ test/test.java
@@ -4,9 +4,9 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("ef");
 		System.out.println("abcd");
+		System.out.println("abcd");
 		System.out.println("미유최고");
 		System.out.println("알콜 마렵다.");
-		System.out.println("asdf");
 	}
 }
 
