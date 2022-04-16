3a63e1f1de90ade7f52cc12fbb3c5506a81ad4a3
monya-9 <lee99714@naver.com>
2022-04-17 03:17:27 +0900

    ssss

--- test/test.java
+++ test/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("ef");
 		System.out.println("abcd");
+		System.out.println("abcd");
 		System.out.println("미유최고");
 		System.out.println("알콜 마렵다.");
 	}
