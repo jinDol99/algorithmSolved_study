cea540e4a4903649351ef0d57251d6e5bb862cd6
WDJJH <jeonjonghun0011@gmail.com>
2022-05-09 00:05:50 +0900

    백준 2742번

---
+++ algorithmSolved_study/BJ220509_2742.java
@@ -0,0 +1,12 @@
+package algorithmSolved_study;
+import java.util.*;
+public class BJ220509_2742 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		for (int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+	}
+}
+// 자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
