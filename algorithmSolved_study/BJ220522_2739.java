cf9776722816de5a809b8756e352a4f1b4d3eec9
WDJJH <jeonjonghun0011@gmail.com>
2022-05-22 13:11:41 +0900

    백준 2739번

---
+++ algorithmSolved_study/BJ220522_2739.java
@@ -0,0 +1,10 @@
+package algorithmSolved_study;
+import java.util.*;
+public class BJ220522_2739 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		int N = s.nextInt();
+		for (int i = 1; i <= 9; i++)
+			System.out.println(N + " * " + i + " = " + N*i);
+	}
+}
