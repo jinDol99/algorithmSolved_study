3d75e3e0a250c455e5ebc527cb8283ee4f2da8b9
bon320-18 <bon320-18@DESKTOP-DC5QV4F>
2022-05-23 17:02:19 +0900

    BaekJoon 2163

---
+++ algorithmSolved_study/BJ220523_2163.java
@@ -0,0 +1,16 @@
+package algorithmSolved_study;
+
+import java.util.Scanner;
+
+public class BJ220523_2163 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int mincou = a*b-1;
+		System.out.println(mincou);
+	
+	}
+
+}
