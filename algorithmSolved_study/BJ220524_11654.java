e8ee5c0580f4ffb23c6614a117caaeeb7e7a4ebc
WDJJH <jeonjonghun0011@gmail.com>
2022-05-24 13:26:07 +0900

    백준 11654번

---
+++ algorithmSolved_study/BJ220524_11654.java
@@ -0,0 +1,9 @@
+package algorithmSolved_study;
+import java.util.*;
+public class BJ220524_11654 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		char W = s.next().charAt(0);
+		System.out.println((int)W);
+	}
+}
