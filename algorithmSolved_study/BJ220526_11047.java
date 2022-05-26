03ff7bd4e559c795073487a9ffcc459d1047e86c
WDJJH <jeonjonghun0011@gmail.com>
2022-05-26 11:30:02 +0900

    백준 11047번 수정 및 해설 추가

--- algorithmSolved_study/BJ220526_11047.java
+++ algorithmSolved_study/BJ220526_11047.java
@@ -6,24 +6,23 @@ public class BJ220526_11047 {
 		int N = s.nextInt();
 		int K = s.nextInt();
 		int[] arr = new int[N];
-		int cnt = 0;
-		for (int i = 0; i < arr.length; i++)
+		int cnt = 0; // 필요한 동전의 최저 갯수
+		for (int i = 0; i < arr.length; i++) // 동전 입력
 			arr[i] = s.nextInt();
-		for (int i = N-1; i >= 0; i--) {
+		for (int i = arr.length-1; i >= 0; i--) {
 			if (K-arr[i] < 0)
-				continue;
-			if (K-arr[i] >= 0) {
-				while (K-arr[i] >= 0) {
+				continue; // 주어진 가격보다 동전이 더 비싸면 스킵
+			if (K-arr[i] >= 0)
+				while (K-arr[i] >= 0) { // 주어진 가격보다 동전이 덜 비쌈
 					K -= arr[i];
 					cnt++;
-					if (K < 0) {
-						K += arr[i];
-						cnt--;
-						break;
-					}
 				}
-			}
 		}
 		System.out.println(cnt);
 	}
 }
+/*
+준규가 가지고 있는 동전은 총 N종류이고, 각각의 동전을 매우 많이 가지고 있다.
+동전을 적절히 사용해서 그 가치의 합을 K로 만들려고 한다.
+이때 필요한 동전 개수의 최솟값을 구하는 프로그램을 작성하시오.
+*/
