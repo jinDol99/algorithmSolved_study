eb41b22493abbae67b431efda2623a571c24238d
kdk8672 <kdk8672@naver.com>
2022-06-01 15:20:30 +0900

    ㅁㅁ

--- BrickBreaker/StartGame.java
+++ BrickBreaker/StartGame.java
@@ -37,16 +37,16 @@ public class StartGame extends JFrame {
 		
 	}
 	public void homeframe() {
-		setTitle("1");//타이틀
-		setSize(1000,700);//프레임의 크기
-		setResizable(false);//창의 크기를 변경하지 못하게
-		setLocationRelativeTo(null);//창이 가운데 나오게
-		setLayout(null);//레이아웃을 내맘대로 설정가능하게 해줌.
-		setVisible(true);//창이 보이게	
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//JFrame이 정상적으로 종료되게
+		setTitle("1"); 
+		setSize(1000,700);
+		setResizable(false);
+		setLocationRelativeTo(null);
+		setLayout(null);
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
-	public void paint(Graphics g) {//그리는 함수
-		g.drawImage(background, 0, 0, null);//background를 그려줌
+	public void paint(Graphics g) {
+		g.drawImage(background, 0, 0, null);
 	}
 	public static void main(String[] args) {
 		 new StartGame();
